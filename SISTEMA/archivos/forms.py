9f48ea75aede4b56d2b9587823630534fc66f5cd
ignacoll21 <21460546@colima.tecnm.mx>
2026-03-03 22:32:36 -0600

    Removiendo db.sqlite3 del repo

--- SISTEMA/archivos/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Archivo
-
-class ArchivoForm(forms.ModelForm):
-    class Meta:
-        model = Archivo
-        fields = ['nombre', 'descripcion', 'archivo']
--- SISTEMA/archivos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.lista_archivos, name='lista'),
-    path('subir/', views.subir_archivo, name='subir'),
-    path('editar/<int:pk>/', views.editar_archivo, name='editar'),
-    path('eliminar/<int:pk>/', views.eliminar_archivo, name='eliminar'),
-]
