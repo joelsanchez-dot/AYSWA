75d416abc9cd8487da820bd72e887c261a5d25e6
angel <mariopadilla2f@gmail.com>
2026-03-04 16:37:54 -0600

    Entrega limpia: Módulo de cupones sin archivos temporales

--- SISTEMA/SISTEMA/settings.py
+++ SISTEMA/SISTEMA/settings.py
@@ -36,8 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'alumnos',
     'django.contrib.staticfiles',
+    'alumnos',
+    'cupones',
 ]
 
 MIDDLEWARE = [
@@ -106,13 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
