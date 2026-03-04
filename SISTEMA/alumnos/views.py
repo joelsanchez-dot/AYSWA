4f8f357083c1dd282232eea4db2e8932f49d1b0b
EduYaye2003 <21460246@colima.tecnm.mx>
2026-03-04 15:55:07 -0600

    Solución de conflictos y modelos de citas listos

--- SISTEMA/alumnos/views.py
+++ SISTEMA/alumnos/views.py
@@ -1,29 +1,7 @@
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from .models import Cita
+from django.shortcuts import render
+from .models import Paciente, Cita
 
-# LISTAR CITAS (Lectura)
-class CitaListView(ListView):
-    model = Cita
-    template_name = 'alumnos/lista_citas.html'
-    context_object_name = 'citas'
-
-# CREAR CITA (Alta)
-class CitaCreateView(CreateView):
-    model = Cita
-    template_name = 'alumnos/form_cita.html'
-    fields = ['paciente', 'fecha', 'hora', 'motivo', 'estado']
-    success_url = reverse_lazy('lista_citas')
-
-# EDITAR CITA (Modificar)
-class CitaUpdateView(UpdateView):
-    model = Cita
-    template_name = 'alumnos/form_cita.html'
-    fields = ['paciente', 'fecha', 'hora', 'motivo', 'estado']
-    success_url = reverse_lazy('lista_citas')
-
-# ELIMINAR CITA (Baja)
-class CitaDeleteView(DeleteView):
-    model = Cita
-    template_name = 'alumnos/confirmar_eliminar.html'
-    success_url = reverse_lazy('lista_citas')
+def lista_citas(request):
+    citas = Cita.objects.all()
+    # Asegúrate de que el HTML esté en alumnos/templates/alumnos/lista.html
+    return render(request, 'alumnos/lista.html', {'citas': citas})
